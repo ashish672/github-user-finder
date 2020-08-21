d6e8f3a2fbf55ea64658a87192aad502fd2bc115
Ashish <ashishchaturvedi672@gmail.com>
2020-08-21 07:16:43 +0530

    Updates

---
+++ precache-manifest.8106eed6aed7f6ee6678d0adf40ce3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8005103546e15096f5c96f2bd42cf763",
+    "url": "/github-user-finder/index.html"
+  },
+  {
+    "revision": "6f5396777421f6e1a346",
+    "url": "/github-user-finder/static/js/2.b1bfd8c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-user-finder/static/js/2.b1bfd8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de372cdecd9bfb86ccb",
+    "url": "/github-user-finder/static/js/main.2b95adef.chunk.js"
+  },
+  {
+    "revision": "03460639c72821e16096",
+    "url": "/github-user-finder/static/js/runtime-main.3c765573.js"
+  }
+]);
